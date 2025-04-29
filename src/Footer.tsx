457e85315d8a83085155bba2c814f93cae8864aa
franzieYoogan <franzieyoogan@gmail.com>
2025-04-29 16:15:32 -0300

    footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,12 @@
-import './assets/css/footer.css'
+import './assets/css/footer.css';
+import twitter from './assets/images/icons/twitter.png';
+import instagram from './assets/images/icons/instagram.png';
+import linkedin from './assets/images/icons/linkedin.png';
+import { useState } from 'react';
 
 function Footer() {
+
+    const [hoveredIcon, setHoveredIcon] = useState<string | null>(null);
 
     return (
         <footer className="footerStyle">
@@ -10,11 +16,31 @@ function Footer() {
             <p>Powered by Squarespace</p>
             </div>
 
-            <div>
+            <div className='containerSocials'>
 
-            <div><img src="" alt="" /></div>
-            <div><img src="" alt="" /></div>
-            <div><img src="" alt="" /></div>
+            <div
+                onMouseEnter={() => setHoveredIcon('twitter')}
+                onMouseLeave={() => setHoveredIcon(null)}
+            >
+                <img src={twitter} className={`twitterStyle socialIcon 
+                    ${hoveredIcon && hoveredIcon !== 'twitter' ? 'dimmed' : ''}`} alt="twitter" />
+            </div>
+
+            <div onMouseEnter={() => setHoveredIcon('instagram')}
+                onMouseLeave={() => setHoveredIcon(null)}>
+
+                <img src={instagram} className={`instagramStyle socialIcon 
+                    ${hoveredIcon && hoveredIcon !== 'instagram' ? 'dimmed' : ''}`} alt="instagram" />
+
+                </div>
+
+            <div onMouseEnter={() => setHoveredIcon('linkedin')}
+                onMouseLeave={() => setHoveredIcon(null)}>
+
+                <img src={linkedin} className={`linkedinStyle socialIcon
+                    ${hoveredIcon && hoveredIcon !== 'linkedin' ? 'dimmed' : ''}`} alt="linkedin" />
+
+                </div>
 
 
             </div>
