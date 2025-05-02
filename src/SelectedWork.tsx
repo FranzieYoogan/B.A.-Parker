8a125b84b19f5fe2033cffc72d1510a1eab7301e
franzieYoogan <franzieyoogan@gmail.com>
2025-05-02 14:16:12 -0300

    selectedWorkDesigns

--- src/SelectedWork.tsx
+++ src/SelectedWork.tsx
@@ -3,6 +3,9 @@ import './assets/css/selectedWork.css'
 import codeSwitch from './assets/images/selectedWork/codeSwitch.jpeg'
 import bbcBlack from './assets/images/selectedWork/bbcBlack.jpeg'
 import minutes from './assets/images/selectedWork/minute.jpeg'
+import cut from './assets/images/selectedWork/cut.png'
+import america from './assets/images/selectedWork/america.png'
+import npr from './assets/images/selectedWork/npr.jpeg'
 
 function selectedWork() {
 
@@ -26,17 +29,17 @@ function selectedWork() {
                     
                     <p className="codeSwitchText">
 
-                    100 years of immigration policies working to keep out immigrants <span className="dateCodeSwitch"> (Jun 5, 2024)</span> <br /> <br />
+                    100 years of immigration policies working to keep <br /> out immigrants <span className="dateCodeSwitch"> (Jun 5, 2024)</span> <br /> <br />
 
-The long, storied history of solidarity between Black and Irish activists <span className="dateCodeSwitch"> (Apr 10, 2024)</span> <br /> <br />
+The long, storied history of solidarity between <br /> Black and Irish activists <span className="dateCodeSwitch"> (Apr 10, 2024)</span> <br /> <br />
 
 Where are the Black punks now? <span className="dateCodeSwitch"> (Nov 1, 2023)</span> <br /> <br />
 
-Student activists are pushing back against big polluters — and winning <span className="dateCodeSwitch"> (Oct 4, 2023)</span> <br /> <br />
+Student activists are pushing back against big <br /> polluters — and winning <span className="dateCodeSwitch"> (Oct 4, 2023)</span> <br /> <br />
 
-Going back to the plantation where my ancestors were enslaved Pt. 2 <span className="dateCodeSwitch"> (Jun 28, 2023)</span> <br /> <br />
+Going back to the plantation where my ancestors <br /> were enslaved Pt. 2 <span className="dateCodeSwitch"> (Jun 28, 2023)</span> <br /> <br />
 
-How do descendants of slavery honor their ancestors' legacy? Pt. 1 <span className="dateCodeSwitch"> (Jun 21, 2023)</span>
+How do descendants of slavery honor their <br /> ancestors' legacy? Pt. 1 <span className="dateCodeSwitch"> (Jun 21, 2023)</span>
 
                     </p>
                     </div>
@@ -49,7 +52,7 @@ How do descendants of slavery honor their ancestors' legacy? Pt. 1 <span classNa
                     
                     <div>
 
-        <p className="pBbcBlack"><span className="spanBbcBlack"> Sport, racism and protests are about to change the lives of “the Black 14” American football players at the University of Wyoming in 1969.</span> <br /> <br />
+        <p className="pBbcBlack"><span className="spanBbcBlack"> Sport, racism and protests are about to change <br /> the lives of “the Black 14” American football players at <br /> the University of Wyoming in 1969.</span> <br /> <br />
 
 Ep. 1 <br /> <br />
 
@@ -73,25 +76,104 @@ Ep. 4</p> <br /> <br />
 
                     <div>
 
-        <p className="pMinutes">Staying grounded after Roe v. Wade; plus, let’s talk about fat liberation <span className="dateCodeSwitch"> (Jul 1, 2022) </span> <br /> <br />
+        <p className="pMinutes">Staying grounded after Roe v. Wade; plus, let’s talk <br /> about fat liberation <span className="dateCodeSwitch"> (Jul 1, 2022) </span> <br /> <br />
 
-When parents say sorry on-screen  (Jun 28, 2022) 
+When parents say sorry on-screen  (Jun 28, 2022) <br /> <br /> 
 
-Guess who’s back in the house (music scene); plus, ‘Would it Kill You to Laugh’ <span className="dateCodeSwitch"> (Jun 24, 2022) </span> <br /> <br />
+Guess who’s back in the house (music scene); plus, <br /> ‘Would it Kill You to Laugh’ <span className="dateCodeSwitch"> (Jun 24, 2022) </span> <br /> <br />
 
-Fangirls rule the internet in 'Everything I Need, I Get From You' <span className="dateCodeSwitch"> (Jun 21, 2022)</span> <br /> <br />
+Fangirls rule the internet in 'Everything I Need, I <br /> Get From You' <span className="dateCodeSwitch"> (Jun 21, 2022)</span> <br /> <br />
 
-Why old is new again in pop music; plus, ‘Rutherford Falls' <span className="dateCodeSwitch"> (Jun 17, 2022) </span> <br /> <br />
+Why old is new again in pop music; plus, <br /> ‘Rutherford Falls' <span className="dateCodeSwitch"> (Jun 17, 2022) </span> <br /> <br />
 
-Joseph Han on U.S. imperialism, Korean ghosts and Guy Fieri <span className="dateCodeSwitch"> (Jun 14, 2022) </span> <br /> <br />
+Joseph Han on U.S. imperialism, Korean ghosts <br /> and Guy Fieri <span className="dateCodeSwitch"> (Jun 14, 2022) </span> <br /> <br />
 
-Even influencers are burning out: plus, there’s nothing boring about ‘Normal Gossip’ <span className="dateCodeSwitch"> (Jun 10, 2022)</span></p> <br /> <br />
+Even influencers are burning out: plus, there’s <br /> nothing boring about ‘Normal Gossip’ <span className="dateCodeSwitch"> (Jun 10, 2022)</span></p> <br /> <br />
 
                     </div>
 
 
                 </div>
 
+
+
+            </section>
+
+            <section className="containerSelectedWork2">
+
+
+                <div>
+
+        <div className="imgSelectedWork">
+
+        <img src={cut} alt="" />
+
+        </div>
+
+        <div>
+
+        <p className="codeSwitchText">In Search of Himbos <span className="dateCodeSwitch"> (Dec 29, 2021) - Figuring out if himbos exist outside of fiction.</span> <br /> <br />
+
+You and I Are in a Parasocial Relationship <span className="dateCodeSwitch"> (Oct 27, 2021) - Understanding what it really means to know one another on the internet.</span> <br /> <br />
+
+Who Gets to Be Safe at HBCUs? <span className="dateCodeSwitch"> (Sept 29, 2021) - A talk with Anita Hill about believing survivors of assault.</span> <br /> <br />
+
+Why Does the Internet Hate Black Women? <span className="dateCodeSwitch">  (Aug 4, 2021) - A look at the growing trend of misogynoir online.</span> <br /> <br />
+
+Why Try? <span className="dateCodeSwitch"> (Jun 16, 2021) - The art of putting yourself out there.</span> <br /> <br />
+
+Does This Poster Make Me Look Racist? <span className="dateCodeSwitch"> (Apr 21, 2021) - Figuring out what to do with all of the problematic art.</span> <br /> <br />
+
+Get Out of Your Head <span className="dateCodeSwitch"> (Mar 10, 2021) - Trying to escape the stress of one year in a pandemic. </span> <br /> <br />
+
+Your Friendly Black Sidekick <span className="dateCodeSwitch"> (Jan 13, 2021) - An ode to ‘90s black child actresses. </span> <br /> <br />
+
+Protesting Without Rules <span className="dateCodeSwitch"> (Oct 21, 2020) - Exploring the ins and outs of protesting.</span></p>
+
+        </div>
+
+                </div>
+
+
+                <div>
+
+                    <div className="imgSelectedWork">
+
+                        <img src={america} alt="" />
+
+                    </div>
+
+                    <div>
+
+                        <p className="codeSwitchText">Name. Age. Detail. <span className="dateCodeSwitch"> (Aug 12, 2022) - Ten people were <br />  killed at a grocery store in Buffalo, NY. Their stories, <br /> as you’ve never heard them.</span> <br /> <br />
+
+Their Eyes Weren’t Watching God <span className="dateCodeSwitch"> (Feb 28, 2020) - <br /> Started going to a church in Harlem and loved it. <br /> There’s just one problem. </span> <br /><br />
+
+The Miseducation of Castlemont High <span className="dateCodeSwitch"> (Apr 27, 2018) - <br /> In 1994, a group of Black and Latinx teens caused a <br /> stir at their high school all because of Schindler’s List.</span> <br /> <br />
+
+History is Not a Toy <span className="dateCodeswitch"> (Oct 6, 2017) - Revisiting a <br /> childhood museum called the Great Blacks in Wax. </span> </p> <br /> <br />
+
+                    </div>
+
+                </div>
+
+                <div>
+
+                    <div className="imgSelectedWork">
+
+                        <img src={npr} alt="" />
+
+                    </div>
+
+                    <div>
+
+                        <p className="codeSwitchText">Love and Lapses <span className="dateCodeSwitch"> (Nov 22, 2019) - Being remembered <br /> and forgotten by the ones you love. </span> <br /> <br />
+
+The End of Empathy <span className="dateCodeSwitch"> (Apr 12, 2019) -  A story told <br /> twice to ask: who deserves our empathy?</span> </p> 
+
+                    </div>
+
+                </div>
 
 
             </section>
