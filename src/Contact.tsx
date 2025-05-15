2018bef64fbc2d1ed9cc0736051c347a410befc2
unknown <franzieyoogan@gmail.com>
2025-05-14 22:30:35 -0300

    contactDonePart1

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,125 +1,60 @@
-import './assets/css/contact.css'
+import './assets/css/contact.css';
+import Footer from './footer';
+
 function Contact() {
+  return (
+    <>
+      <section className='containerAllContact'>
+        <div className='formWrapper'>
 
-    return(
+          <div className='contactTitle'>Contact</div>
+          <div className='howItGoing'>How's it going?</div>
 
-        <>
-        
-        <section className='containerAllContact'>
+          <form>
+            {/* Name Section */}
+            <div className='containerName'>
+              <div className='firstNameContainer'>
+                <h1 className='titleForm'>Name <span>(required)</span></h1>
+                <label htmlFor="firstName" className='labelName'>First Name</label>
+                <input type="text" className='inputName' id='firstName' name='firstName' />
+              </div>
 
-
-        <div>
-
-        
-        <div className='contactTitle'>Contact</div>
-
-        <div className='howItGoing'>How's it going?</div>
-
-        <div className='containerName'>
-        
-        <div>
-
-        <div><h1 className='titleForm'>Name <span>(required)</span></h1></div>
-        
-            <div>
-
-            
-        <div>
-
-        <label htmlFor="firstName" className='labelName'>First Name</label>
-    
-        </div>
-
-        <div> <input type="text" className='inputName' id='firstName' name='firstName' /></div>
-
-        </div>
-
-        </div>
-
-
-        <div className='containerLastName'>
-
-
-        <div>
-
-            <label htmlFor="lastName" className='labelName'>Last Name</label>
-            
-        </div>
-
-            <div>
-
-            <input type="text" id='lastName' name='lastName' className='inputName' />
-
+              <div className='firstNameContainer'>
+                <h1 className='titleForm'>&nbsp;</h1> {/* placeholder for alignment */}
+                <label htmlFor="lastName" className='labelName'>Last Name</label>
+                <input type="text" className='inputName' id='lastName' name='lastName' />
+              </div>
             </div>
-        
 
+            {/* Email */}
+            <section className='containerEmail'>
+              <label className='titleForm labelEmail' htmlFor='email'>Email <span>(required)</span></label>
+              <input className='inputEmail' id='email' name='email' type="email" />
+            </section>
+
+            {/* Subject */}
+            <section className='containerEmail'>
+              <label className='titleForm labelEmail' htmlFor='subject'>Subject <span>(required)</span></label>
+              <input className='inputEmail' id='subject' name='subject' type="text" />
+            </section>
+
+            {/* Message */}
+            <section className='containerEmail'>
+              <label className='titleForm labelEmail' htmlFor='message'>Message <span>(required)</span></label>
+              <textarea className='inputEmail' name="message" id="message"></textarea>
+            </section>
+
+            {/* Submit Button */}
+            <div className='containerButton'>
+              <button className='buttonSubmit' type='submit'>SUBMIT</button>
+            </div>
+          </form>
         </div>
-        
+      </section>
 
-        </div>
-
-        
-        
-        <section className='containerEmail'>
-
-
-        <div>
-
-        <div><label className='titleForm labelEmail' htmlFor='email'>Email <span>(required)</span></label></div>
-
-        </div>
-
-        <div><input className='inputEmail' id='email' name='email' type="email" /></div>
-
-        </section>
-
-
-
-
-         <section className='containerEmail'>
-
-
-        <div>
-
-        <div><label className='titleForm labelEmail' htmlFor='subject'>Subject <span>(required)</span></label></div>
-
-        </div>
-
-        <div><input className='inputEmail' id='subject' name='subject' type="text" /></div>
-
-        </section>
-
-
-
-         <section className='containerEmail'>
-
-
-        <div>
-
-        <div><label className='titleForm labelEmail' htmlFor='message'>Message <span>(required)</span></label></div>
-
-        </div>
-
-        <div><textarea className='inputEmail' name="message" id="message"></textarea></div>
-
-        </section>
-
-        
-        <div className='containerButton'>
-
-        <div><button className='buttonSubmit' type='submit'>SUBMIT</button></div>
-
-        </div>
-
-
-        </div>
-
-        </section>
-
-        </>
-
-    )
-
+      <Footer />
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
